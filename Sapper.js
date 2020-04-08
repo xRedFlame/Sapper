49140f8ff0e01d82ef780d0446b066140b94b87d
spades <usv@intelins.ru>
2020-04-08 22:06:04 +0300

    spades` commit

--- Sapper.js
+++ Sapper.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function () {
-
+    // spades` comment
     let newGame = document.querySelector('.new-game');
     newGame.onClick = function () {
         alert("click");
